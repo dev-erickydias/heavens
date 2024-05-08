8a5ac087da639dde64891bdf5c6b02062aa763e3
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-08 09:23:12 +0100

    add modificações index global e config css alem de add component precos e servicos

---
+++ src/components/catalogoPrecos/Precos.jsx
@@ -0,0 +1,52 @@
+import "./precos.css"
+
+function Precos() {
+    return (
+        <ul className="precos">
+            <li className="precos__item">
+                <p className="precos__item__text">Protese Capilar</p>
+                <div className="precos__item__info">
+                    <p className="precos__item__preco">400€</p>
+                    <p className="precos__item__preco">400€</p>
+                </div>
+            </li>
+            <li className="precos__item">
+                <p className="precos__item__text">Protese Capilar</p>
+                <div className="precos__item__info">
+                    <p className="precos__item__preco">400€</p>
+                    <p className="precos__item__preco">400€</p>
+                </div>
+            </li>
+            <li className="precos__item">
+                <p className="precos__item__text">Protese Capilar</p>
+                <div className="precos__item__info">
+                    <p className="precos__item__preco">400€</p>
+                    <p className="precos__item__preco">400€</p>
+                </div>
+            </li>
+            <li className="precos__item">
+                <p className="precos__item__text">Protese Capilar</p>
+                <div className="precos__item__info">
+                    <p className="precos__item__preco">400€</p>
+                    <p className="precos__item__preco">400€</p>
+                </div>
+            </li>
+            <li className="precos__item">
+                <p className="precos__item__text">Protese Capilar</p>
+                <div className="precos__item__info">
+                    <p className="precos__item__preco">400€</p>
+                    <p className="precos__item__preco">400€</p>
+                </div>
+            </li>
+            <li className="precos__item">
+                <p className="precos__item__text">Protese Capilar</p>
+                <div className="precos__item__info">
+                    <p className="precos__item__preco">400€</p>
+                    <p className="precos__item__preco">400€</p>
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+export default Precos
