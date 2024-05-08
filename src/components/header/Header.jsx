b2c05971187fed40035a7b163b6e8e64e9d51bb5
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-08 08:52:23 +0100

    modificações em estilos e de position do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,15 +9,21 @@ function Header() {
     return (
         <header className="header">
             <Navigation />
-            <img src={imgBackGound} alt="imgBackGound" className="imgBackGound" />
+            
 
-            <div className="titles">
-                <h1 className="title">Desperte sua autenticidade!</h1>
-                <h2 className="subtitle">Liberte sua beleza com estilo e confiança.</h2>
-            </div>
-            <div className="buttons">
-                <button className="btn__agenda">Agende aqui</button>
-                <button className="btn__contacto">Entre em contato</button>
+            <div className="header__container">
+            
+                <div className="contein">
+                    <div className="titles">
+                        <h1 className="title">Desperte sua autenticidade!</h1>
+                        <h2 className="subtitle">Liberte sua beleza com estilo e confiança.</h2>
+                    </div>
+                    <div className="buttons">
+                        <button className="btn__agenda">Agende aqui</button>
+                        <button className="btn__contacto">Entre em contato</button>
+                    </div>
+                </div>
+                <img src={imgBackGound} alt="imgBackGound" className="imgBackGound" />
             </div>
         </header>
     )
