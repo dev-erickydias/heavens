5906ef000b5136b7d6cc982d4b04f7b7f500f879
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-24 15:40:33 +0100

    add funcao da Api

---
+++ src/utils/Api.js
@@ -0,0 +1,25 @@
+
+
+
+export default async function Api(){
+    const url = 'https://api.zappysoftware.com/1/agentes/getsingle.json?page=1&id=343025&via_app=1&lang=en-US&professionalMode=false';
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data.getsingle?.[0].estabelecimentos?.[0].servicos;
+    } catch (error) {
+        console.error('Error fetching agent data:', error.message);
+        return null;
+    }
+
+}
+
+
+
