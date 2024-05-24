0ebd50bc66af064598200c1bf6ded9d0871523a5
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-24 15:40:42 +0100

    add component Header

---
+++ src/components/header/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import NavMenu from "../navMenu/NavMenu";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <NavMenu />
+    </header>
+  );
+}
